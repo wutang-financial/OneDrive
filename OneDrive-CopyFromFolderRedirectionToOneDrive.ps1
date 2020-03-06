7a3e5fb411f31531f227bce2674425b6731ec7ce
Deyda <59874195+Deyda@users.noreply.github.com>
2020-03-06 13:15:23 +0100

    Update and rename CopyFromFolderRedirectionToOneDrive.ps1 to OneDrive-CopyFromFolderRedirectionToOneDrive.ps1

--- OneDrive-CopyFromFolderRedirectionToOneDrive.ps1
+++ OneDrive-CopyFromFolderRedirectionToOneDrive.ps1
@@ -1,4 +1,16 @@
-#The script copies the existing folders "desktop" and "documents" into the onedrive directory
+<#
+.SYNOPSIS
+The script copies the existing folders "desktop" and "documents" into the onedrive directory
+
+.DESCRIPTION
+Use this script to copy the Folder Redirection Folder Desktop and Documents local and fetch them into OneDrive Known Folder
+.NOTES
+  Version:        1.0
+  Author:         Manuel Winkel <www.deyda.net>
+  Creation Date:  2020-03-04
+  Purpose/Change:
+#>
+
 $Documents = [environment]::getfolderpath("mydocuments")
 $Desktop = [environment]::getfolderpath("desktop")
 $Target = $env:OneDriveCommercial
